0145d0a1c715d71b265be46b16da1cb5ebe59954
mattbocc <matthew.boccalon@live.ca>
2025-10-20 20:01:53 -0400

    updates to table schemas by adding drug synonyms, chembl mechanism, chembl drug data, and cellosaurus cell lines. Major reworka and refactor of pubchem drug extraction script. Added cellosaurus extreaction script.

--- seeding/seeding_coordinator.py
+++ seeding/seeding_coordinator.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
-from create_tables import Base
 from urllib.parse import quote_plus
 import pandas as pd
 
+from create_tables import Base, Compounds, CompoundSynonyms, ChemblMechanism, CellLines
 
 load_dotenv(override=True)
 
@@ -18,9 +18,38 @@ engine = create_engine(
 Base.metadata.create_all(engine)
 
 
-df = pd.read_csv(
-    "../data_extraction/drugs/pubchem/output_data/pubchem_table_omitted.csv"
-)
-df = df.drop(columns=[df.columns[0]])  # Remove first empty column
+# Helper to align a dataframe to a model's columns
+def align_to_model(df: pd.DataFrame, model) -> pd.DataFrame:
+    cols = [c.name for c in model.__table__.columns]
+    return df[[c for c in cols if c in df.columns]]
 
-df.to_sql(name="pubchem", con=engine, if_exists="append", index=False)
+
+# Read the attached files directly
+compounds_df = pd.read_csv("data_extraction/drugs/pubchem/output_data/drug_out.csv")
+synonyms_df = pd.read_csv("data_extraction/drugs/pubchem/output_data/drug_synonyms.csv")
+chembl_mech_df = pd.read_csv(
+    "data_extraction/drugs/pubchem/output_data/chembl_mechanism.csv"
+)
+cell_lines_df = pd.read_csv(
+    "data_extraction/cell_lines/cellosaurus/output_data/cell_lines_table_cleaned.csv"
+)
+
+# Align columns to ORM models
+compounds_df = align_to_model(compounds_df, Compounds)
+synonyms_df = align_to_model(synonyms_df, CompoundSynonyms)
+chembl_mech_df = align_to_model(chembl_mech_df, ChemblMechanism)
+cell_lines_df = align_to_model(cell_lines_df, CellLines)
+
+# Insert into tables named by the ORM models
+compounds_df.to_sql(
+    name=Compounds.__tablename__, con=engine, if_exists="append", index=False
+)
+synonyms_df.to_sql(
+    name=CompoundSynonyms.__tablename__, con=engine, if_exists="append", index=False
+)
+chembl_mech_df.to_sql(
+    name=ChemblMechanism.__tablename__, con=engine, if_exists="append", index=False
+)
+cell_lines_df.to_sql(
+    name=CellLines.__tablename__, con=engine, if_exists="append", index=False
+)
